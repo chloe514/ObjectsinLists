f04aa59053c7cc3bef834414e21f1842fb8dd0dd
chloe514 <chloevans22@gmail.com>
2024-07-25 16:17:49 -0400

    made changes

--- src/TVShow.java
+++ src/TVShow.java
@@ -1,34 +1,62 @@
-public class TVShow {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class TvShow {
     private String showName;
     private int numberOfEpisodes;
     private String genre;
 
-    // Constructor
-    public TVShow(String showName, int numberOfEpisodes, String genre) {
+    public TvShow(String showName, int numberOfEpisodes, String genre) {
         this.showName = showName;
         this.numberOfEpisodes = numberOfEpisodes;
         this.genre = genre;
     }
 
-    // Getter for show name
     public String getShowName() {
         return showName;
     }
 
-    // Getter for number of episodes
     public int getNumberOfEpisodes() {
         return numberOfEpisodes;
     }
 
-    // Getter for genre
     public String getGenre() {
         return genre;
     }
 
-    // toString method to print TV show details
     @Override
     public String toString() {
-        return "Show Name: " + showName + ", Number of Episodes: " + numberOfEpisodes + ", Genre: " + genre;
+        return "Show Name: " + getShowName() + ", Episodes: " + getNumberOfEpisodes() + ", Genre: " + getGenre();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<TvShow> tvShows = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter the TV show name (or press Enter to stop): ");
+            String showName = scanner.nextLine();
+            if (showName.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Enter the number of episodes: ");
+            int numberOfEpisodes = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            System.out.print("Enter the genre: ");
+            String genre = scanner.nextLine();
+
+            tvShows.add(new TvShow(showName, numberOfEpisodes, genre));
+        }
+
+        System.out.println("TV Show List:");
+        for (TvShow show : tvShows) {
+            System.out.println(show);
+        }
+
+        scanner.close();
     }
 }
+
 
